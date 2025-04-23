7f1cbc442ef1b73ae6651afa5711daede68d312e
EngineerMoore <alexiashalise@gmail.com>
2025-04-23 14:40:22 -0500

    called cors after the body parser to fix cannot destructure error

--- src/server.js
+++ src/server.js
@@ -6,9 +6,9 @@ const cors = require('cors');
 const app = express();
 
 const PORT = process.env.PORT || 4000;
-app.use(cors());
 
-app.use(express.json())
+app.use(express.json());
+app.use(cors());
 
 app.use(require('morgan')('dev'))
 
